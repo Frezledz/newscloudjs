7c165d5e8f5de934568d4ae2c0ed13a735b7cd66
xxXFreezerXxx <freezerfx512@gmail.com>
2022-11-01 21:49:13 +0000

    commits.

--- libraries/login.js
+++ libraries/login.js
@@ -1,6 +1,7 @@
 const https = require("https");
+const adatas = require("./setdata").adatas;
 let _tokens = {sessionsid:"",token:"",isloggedin:0};
-const _login = (name,pass)=>{
+const _login = ()=>{
  return new Promise((resolve) =>{
    const reqoptions = {
        hostname: 'scratch.mit.edu',
@@ -13,7 +14,7 @@ const _login = (name,pass)=>{
        },
        method: "POST"
      }
-     const data = JSON.stringify({ username:name, password:pass, useMessages: true })
+     const data = JSON.stringify({ username:adatas.username, password:adatas.password, useMessages: true })
      const req = https.request(reqoptions, res => {
 
       let datas = [];
@@ -23,13 +24,13 @@ const _login = (name,pass)=>{
          res.on("end",()=>{
           for(c of res.rawHeaders){             
             if(c.includes("scratchsessionsid")){
-              tokens.sessionsid = c.match(/\"(.*)\"/g)[0];
+              _tokens.sessionsid = c.match(/\"(.*)\"/g)[0];
             break;
           }
           }
           const a = Buffer.concat(datas);
           const reg = JSON.parse(Buffer.from(a,"utf-8").toString())
-          tokens.token = reg[0].token;
+          _tokens.token = reg[0].token;
           resolve(tokens);
          })
      })
--- libraries/setdata.js
+++ libraries/setdata.js
@@ -1,8 +1,8 @@
 let _adatas = {username:"",password:"",projectid:""};
 const _setdatas = (username,password,projectid)=>{
-    adatas.username = username;
-    adatas.password = password;
-    adatas.projectid = projectid;
+    _adatas.username = username;
+    _adatas.password = password;
+    _adatas.projectid = projectid;
 }
 
 module.exports = {adatas:_adatas,setdatas:_setdatas};

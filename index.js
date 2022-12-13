5059175f8c3675398ad44522d0f492e11537ab30
xxxfreezerxxx <freezerfx512@gmail.com>
2022-12-13 23:41:14 +0000

    a

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const _sendtocloud = require("./libraries/cloud").sendtocloud;
 const _login = require("./libraries/login").login;
 const _setdatas = require("./libraries/setdata").setdatas;
 const _parsedata = require("./libraries/cloud").parsedata;
+const _setpredata = require("./libraries/cloud").setpredata;
 module.exports = (
     {
         connect:_connect,
@@ -11,6 +12,7 @@ module.exports = (
         sendtocloud:_sendtocloud,
         login:_login,
         setdatas:_setdatas,
-        parsedata:_parsedata
+        parsedata:_parsedata,
+        setpredata:_setpredata
     }
     )
